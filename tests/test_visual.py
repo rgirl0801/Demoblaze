04b3ea53544616668128d24a2c2e094c5fb8aa3f
KateFox <52407432+rgirl0801@users.noreply.github.com>
2022-12-12 13:48:28 -0500

    add allure

--- tests/test_visual.py
+++ tests/test_visual.py
@@ -1,3 +1,6 @@
+import time
+import allure
+
 import pytest
 
 from pages.main_page import MainPage
@@ -9,6 +12,7 @@ class TestVisualClass:
         self.main_page = MainPage(browser, url)
         self.main_page.open_page()
 
+    @allure.title("Registered user is able to login with valid credentials")
     def test_carousel_next_prev(self):
         self.main_page.samsung_is_present()
         self.main_page.carousel_click_prev()
@@ -18,8 +22,11 @@ class TestVisualClass:
         self.main_page.nexus_is_present()
 
     def test_categories_phones(self):
+        time.sleep(2)
         self.main_page.select_phone_category()
+        time.sleep(2)
         self.main_page.check_qty_phones()
+        time.sleep(2)
 
     def test_footer_is_present(self):
         self.main_page.check_footer_about()
