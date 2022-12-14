b3f91e17036081339bb2bbb913292f318cd813bf
KateFox <52407432+rgirl0801@users.noreply.github.com>
2022-12-14 09:34:49 -0500

    added conftest options

--- conftest.py
+++ conftest.py
@@ -1,19 +1,19 @@
-import pytest
-import Links
-
 import logging
 
+import pytest
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 
+import links
+
 
 @pytest.fixture()
-def browser():
+def browser(request, headless):
     options = webdriver.ChromeOptions()
-    options.headless = True
+    options.headless = headless
     logging.info('start logs')
-    browser = webdriver.Chrome(ChromeDriverManager().install(), options=options)
-    browser.set_window_size(1920, 1080)
+    browser = webdriver.Chrome(ChromeDriverManager().install(), options=options
+    )
     yield browser
     logging.info('end logs')
     browser.quit()
@@ -21,7 +21,25 @@ def browser():
 
 @pytest.fixture(scope="session")
 def url():
-    url = Links.base_url
+    url = links.base_url
     if not url:
         raise Exception("Wrong environment")
     return url
+
+
+def pytest_addoption(parser):
+    parser.addoption(
+        "--launch",
+        default="chrome",
+        help="define browser: chrome or firefox, --browser=chrome",
+    )
+    parser.addoption(
+        "--headless",
+        action="store_true",
+        help="headless mode on or off, --headless",
+    )
+
+
+@pytest.fixture(scope='class')
+def headless(request):
+    return request.config.getoption("--headless")
