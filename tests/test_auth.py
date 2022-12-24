3dd0dad821a28c9e79c3a83e3ceb35f21809b036
KateFox <52407432+rgirl0801@users.noreply.github.com>
2022-12-23 22:35:42 -0500

    visual allure steps

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -1,9 +1,8 @@
-import time
+import allure
 import pytest
 
-from pages.cart_page import CartPage
-from pages.main_page import MainPage
 from constants import POSITIVE_LOGIN_CREDS, POSITIVE_SIGNUP_CREDS, NEGATIVE_LOGIN_CREDS
+from pages.main_page import MainPage
 
 
 class TestAuthClass:
@@ -12,16 +11,19 @@ class TestAuthClass:
         self.main_page = MainPage(browser, url)
         self.main_page.open_page()
 
+    @allure.feature('Login on UI')
     def test_login_ui(self):
         self.main_page.go_to_login()
         self.main_page.login_ui(POSITIVE_LOGIN_CREDS['name'], POSITIVE_LOGIN_CREDS['password'])
         self.main_page.check_login_successfully_complete()
 
+    @allure.feature('Sign up on UI')
     def test_signup_ui(self):
         self.main_page.go_to_signup()
         self.main_page.signup_ui(POSITIVE_SIGNUP_CREDS['name'], POSITIVE_SIGNUP_CREDS['password'])
         self.main_page.check_signup_successfully_complete()
 
+    @allure.feature('Negative login')
     @pytest.mark.parametrize("name, password", NEGATIVE_LOGIN_CREDS,
         ids=["empty name", "empty password",
              "unregistered user"])
